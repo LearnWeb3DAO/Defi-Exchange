a4fff4ab3f5220d82035fc4d4390b457f9b7705e
Nartey Kodjo-Sarso <43901544+narteysarso@users.noreply.github.com>
2022-06-01 16:07:53 +0000

    fix unused import

--- hardhat-tutorial/scripts/deploy.js
+++ hardhat-tutorial/scripts/deploy.js
@@ -1,5 +1,5 @@
 const { ethers } = require("hardhat");
-require("dotenv").config({ path: ".env" });
+
 const { CRYPTO_DEV_TOKEN_CONTRACT_ADDRESS } = require("../constants");
 
 async function main() {
